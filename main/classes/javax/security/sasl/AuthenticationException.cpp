99d3694c192840a2cad26b799448ba210285b6ba
Yao Yuan <yeaya@163.com>
2025-10-14 12:58:04 +0800

    first commit

---
+++ main/classes/javax/security/sasl/AuthenticationException.cpp
@@ -0,0 +1,85 @@
+#include <javax/security/sasl/AuthenticationException.h>
+
+#include <java/lang/Class.h>
+#include <java/lang/ClassInfo.h>
+#include <java/lang/FieldInfo.h>
+#include <java/lang/MethodInfo.h>
+#include <java/lang/String.h>
+#include <java/lang/Throwable.h>
+#include <java/lang/reflect/Constructor.h>
+#include <java/lang/reflect/Method.h>
+#include <javax/security/sasl/SaslException.h>
+#include <jcpp.h>
+
+using $ClassInfo = ::java::lang::ClassInfo;
+using $FieldInfo = ::java::lang::FieldInfo;
+using $MethodInfo = ::java::lang::MethodInfo;
+using $SaslException = ::javax::security::sasl::SaslException;
+
+namespace javax {
+	namespace security {
+		namespace sasl {
+
+$FieldInfo _AuthenticationException_FieldInfo_[] = {
+	{"serialVersionUID", "J", nullptr, $PRIVATE | $STATIC | $FINAL, $constField(AuthenticationException, serialVersionUID)},
+	{}
+};
+
+$MethodInfo _AuthenticationException_MethodInfo_[] = {
+	{"<init>", "()V", nullptr, $PUBLIC, $method(static_cast<void(AuthenticationException::*)()>(&AuthenticationException::init$))},
+	{"<init>", "(Ljava/lang/String;)V", nullptr, $PUBLIC, $method(static_cast<void(AuthenticationException::*)($String*)>(&AuthenticationException::init$))},
+	{"<init>", "(Ljava/lang/String;Ljava/lang/Throwable;)V", nullptr, $PUBLIC, $method(static_cast<void(AuthenticationException::*)($String*,$Throwable*)>(&AuthenticationException::init$))},
+	{}
+};
+
+$ClassInfo _AuthenticationException_ClassInfo_ = {
+	$PUBLIC | $ACC_SUPER,
+	"javax.security.sasl.AuthenticationException",
+	"javax.security.sasl.SaslException",
+	nullptr,
+	_AuthenticationException_FieldInfo_,
+	_AuthenticationException_MethodInfo_
+};
+
+$Object* allocate$AuthenticationException($Class* clazz) {
+	return $of($alloc(AuthenticationException));
+}
+
+void AuthenticationException::init$() {
+	$SaslException::init$();
+}
+
+void AuthenticationException::init$($String* detail) {
+	$SaslException::init$(detail);
+}
+
+void AuthenticationException::init$($String* detail, $Throwable* ex) {
+	$SaslException::init$(detail, ex);
+}
+
+AuthenticationException::AuthenticationException() {
+}
+
+AuthenticationException::AuthenticationException(const AuthenticationException& e) {
+}
+
+AuthenticationException AuthenticationException::wrapper$() {
+	$pendingException(this);
+	return *this;
+}
+
+void AuthenticationException::throwWrapper$() {
+	$pendingException(this);
+	throw *this;
+}
+
+$Class* AuthenticationException::load$($String* name, bool initialize) {
+	$loadClass(AuthenticationException, name, initialize, &_AuthenticationException_ClassInfo_, allocate$AuthenticationException);
+	return class$;
+}
+
+$Class* AuthenticationException::class$ = nullptr;
+
+		} // sasl
+	} // security
+} // javax
---
+++ main/classes/javax/security/sasl/AuthorizeCallback.h
@@ -0,0 +1,42 @@
+#ifndef _javax_security_sasl_AuthorizeCallback_h_
+#define _javax_security_sasl_AuthorizeCallback_h_
+//$ class javax.security.sasl.AuthorizeCallback
+//$ extends javax.security.auth.callback.Callback
+//$ implements java.io.Serializable
+
+#include <java/io/Serializable.h>
+#include <javax/security/auth/callback/Callback.h>
+
+namespace javax {
+	namespace security {
+		namespace sasl {
+
+class $export AuthorizeCallback : public ::javax::security::auth::callback::Callback, public ::java::io::Serializable {
+	$class(AuthorizeCallback, $NO_CLASS_INIT, ::javax::security::auth::callback::Callback, ::java::io::Serializable)
+public:
+	AuthorizeCallback();
+	virtual ::java::lang::Object0* toObject0$() const override {return (::java::lang::Object0*)(void*)this;}
+	virtual $Object* clone() override;
+	virtual bool equals(Object$* arg0) override;
+	virtual void finalize() override;
+	virtual int32_t hashCode() override;
+	void init$($String* authnID, $String* authzID);
+	virtual $String* getAuthenticationID();
+	virtual $String* getAuthorizationID();
+	virtual $String* getAuthorizedID();
+	virtual bool isAuthorized();
+	virtual void setAuthorized(bool ok);
+	virtual void setAuthorizedID($String* id);
+	virtual $String* toString() override;
+	$String* authenticationID = nullptr;
+	$String* authorizationID = nullptr;
+	$String* authorizedID = nullptr;
+	bool authorized = false;
+	static const int64_t serialVersionUID = (int64_t)0xDF573DEE52013E6B;
+};
+
+		} // sasl
+	} // security
+} // javax
+
+#endif // _javax_security_sasl_AuthorizeCallback_h_
